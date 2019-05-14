b9db611612d84661388ce12ad20e5726a2db1a8c
bangn <thebangnguyen@gmail.com>
2019-05-15 00:57:07 +1000

    [#2]: Add basic PBT

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,7 +3,16 @@ module Main
     )
 where
 
+import Control.Monad.Extra (ifM)
+import Hedgehog (Group (..), checkParallel)
+import HedgeHog.Spec (propSquashTwice)
 import Oracle.Spec (testOracles)
+import System.Exit (exitFailure, exitSuccess)
 
 main :: IO ()
-main = testOracles
+main = do
+    testOracles
+    ifM (checkParallel hedgehogTests) exitSuccess exitFailure
+
+hedgehogTests :: Group
+hedgehogTests = Group "Property based test" [("squash twice", propSquashTwice)]
