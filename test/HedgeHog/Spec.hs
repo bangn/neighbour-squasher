7dc2cabe3d2fcac831591b736e37fdabddf90285
bangn <thebangnguyen@gmail.com>
2019-05-21 23:24:52 +1000

    [#2]: Format using floskell

--- test/HedgeHog/Spec.hs
+++ test/HedgeHog/Spec.hs
@@ -1,16 +1,14 @@
-module HedgeHog.Spec
-  ( propSquashTwice
-  ) where
+module HedgeHog.Spec ( propSquashTwice ) where
 
-import Hedgehog (Property, (===), forAll, property)
+import Hedgehog (Property, forAll, property, (===))
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
+
 import NeighbourSquasher.Core (squashNeighbourBy)
 
 propSquashTwice :: Property
-propSquashTwice =
-  property $ do
+propSquashTwice = property $ do
     string <- forAll $ Gen.string (Range.linear 0 100) Gen.unicode
     char <- forAll Gen.unicode
-    squashNeighbourBy char (squashNeighbourBy char string) ===
-      squashNeighbourBy char string
+    squashNeighbourBy char (squashNeighbourBy char string)
+        === squashNeighbourBy char string
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,17 +1,20 @@
-module Main
-  ( main
-  ) where
+module Main ( main ) where
 
 import Control.Monad.Extra (ifM)
+
 import HedgeHog.Spec (propSquashTwice)
-import Hedgehog (Group(..), checkParallel)
+
+import Hedgehog (Group (..), checkParallel)
+
 import Oracle.Spec (testOracles)
+
 import System.Exit (exitFailure, exitSuccess)
 
 main :: IO ()
 main = do
-  testOracles
-  ifM (checkParallel hedgehogTests) exitSuccess exitFailure
+    testOracles
+    ifM (checkParallel hedgehogTests) exitSuccess exitFailure
 
 hedgehogTests :: Group
-hedgehogTests = Group "Property based test" [("squash twice", propSquashTwice)]
+hedgehogTests =
+    Group "Property based test" [ ("squash twice", propSquashTwice) ]
